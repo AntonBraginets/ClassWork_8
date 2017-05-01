f98c4a1f2a1cdb359276c3c5861d3c155b4fee8b
antonbraginets <alpha.india.bravo@gmail.com>
2017-05-01 18:12:55 +0300

    ver. 1.0

--- src/Example2/Ship.java
+++ src/Example2/Ship.java
@@ -1,4 +1,36 @@
 package Example2;
 
-public class Ship {
+class Ship extends Vehicle {
+    @Override
+    public void setPrice(double price) {
+        super.setPrice(30000000);
+    }
+
+    @Override
+    public double getPrice() {
+        return super.getPrice();
+    }
+
+    @Override
+    public void setSpeed(double speed) {
+        super.setSpeed(54);
+    }
+
+    @Override
+    public double getSpeed() {
+        return super.getSpeed();
+    }
+
+    @Override
+    public void setYear(int year) {
+        super.setYear(1999);
+    }
+
+    @Override
+    public int getYear() {
+        return super.getYear();
+    }
+
+    private int passengers = 150;
+    private String port = "Odessa";
 }
